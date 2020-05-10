d82500e66e25182c02cf34e866626511a6860ca7
YOUR NAME <orenchaky@gmail.com>
2020-05-10 17:24:35 +0300

    api

--- server/route/api.js
+++ server/route/api.js
@@ -1,23 +1,58 @@
 const express = require('express')
 const router = express.Router()
 const axios = require('axios')
+const City = require('../../model/City.js')
+const apiKey = "c44198a7c2a2103cd137d8dbb71c63bf";
 
-
-router.get(`/city/:cityName`, async (req, res) => {
-    const { cityName } = req.params
-    await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=c44198a7c2a2103cd137d8dbb71c63bf`)
-        .then(function (response) {
-            res.send(response.data)
-            console.log(response.data);
-        })
-        .catch(function (error) {
-            console.log(error);
-        })
-        .finally(function () {
-            console.log('enter to finally');
-        });
+//returns json with all the cities
+router.get('/cities', async (req, res) => {
+    let cities = await City.find({})
+    res.send(cities)
 })
 
+//geta the weather according to the api
+const getWeather = async function (url) {
+    try {
+        let result = await axios.get(url);
+        return result.data;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+//gets a city by name 
+router.get(`/city/:cityName`, async (req, res) => {
+    let { cityName } = req.params;
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
+    let data = await getWeather(url);
+    data = {
+        name: data.name,
+        temperature: Math.round(data.main.temp),
+        condition: data.weather[0].description,
+        conditionPic: `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+    };
+    res.send(data);
+});
+
+//post city 
+router.post('/city', async (req, res) => {
+    let city = new City(req.body)
+    await city.save()
+    City.find({}, (err, city) => {
+        res.send(city)
+    })
+})
+
+//delete a city by name
+router.delete('/city/:cityName', async (req, res) => {
+    let cityName = req.params.cityName
+    try {
+        await City.findOneAndDelete({ name: `${cityName}` })
+    } catch (error) {
+        console.log(error);
+        res.send(`problem to delete ,${cityName} wasn't deleted`)
+    }
+})
 
 module.exports = router
 
@@ -34,5 +69,7 @@ module.exports = router
             console.log(error);
         }
     })
+
+
 
 */
